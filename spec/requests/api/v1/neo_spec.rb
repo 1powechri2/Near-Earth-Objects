86036107e708f1dd5b8b1036e0b9e4fa2607ccf3
Chris Powell <chris.ian.powell@gmail.com>
2018-12-13 09:19:28 -0700

    Adds gems to gemfile and first test for internal endpoint

---
+++ spec/requests/api/v1/neo_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Near Earth Objects Endpoint' do
+  context '/api/v1/neo?start_date=2018-12-6&end_date=2018-12-12' do
+    it 'returns a collection of near earth object data' do
+      start_date = '2018-12-6'
+      end_date = '2018-12-12'
+      get "/api/v1/neo?start_date=#{start_date}&end_date=#{end_date}"
+
+      expect(response.status).to eq 200
+
+      neos = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
