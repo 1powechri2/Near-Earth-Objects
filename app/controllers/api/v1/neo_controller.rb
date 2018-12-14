80b41dade169c7dcce3bdb485866506041b2f5d2
Chris Powell <chris.ian.powell@gmail.com>
2018-12-13 17:27:01 -0700

    Adds test that verifies check for a bad filter query

--- app/controllers/api/v1/neo_controller.rb
+++ app/controllers/api/v1/neo_controller.rb
@@ -1,11 +1,35 @@
 class Api::V1::NeoController < ApplicationController
   def index
-    render json: NeoPresenter.new(neo_params).neos
+    render_index
   end
 
 private
 
   def neo_params
     params.permit(:start_date, :end_date, :sort, :filter)
+  end
+
+  def check_sort
+    if neo_params[:sort] != nil
+      if neo_params[:sort] == 'magnitude' || neo_params[:sort] == 'speed' || neo_params[:sort] == 'miss_distance' || neo_params[:sort] == 'diameter'
+        render json: NeoPresenter.new(neo_params).neos
+      else
+        render json: {error: 'bad sort query'}, status: 400
+      end
+    else
+      render json: NeoPresenter.new(neo_params).neos
+    end
+  end
+
+  def render_index
+    if neo_params[:filter] != nil
+      if neo_params[:filter] == 'hazard'
+        check_sort
+      else
+        render json: {error: 'filter query must = hazard'}, status: 400
+      end
+    else
+      check_sort
+    end
   end
 end
---
+++ spec/requests/api/v1/neo_filter_and_sort_edgecase_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Bad requests return status errors and json error messages' do
+  context '/api/v1/neo?start_date=2018-12-6&end_date=2018-12-12&filter=stuff&sort=thingy' do
+    it 'returns status 400' do
+      VCR.use_cassette("neos") do
+        get '/api/v1/neo?start_date=2018-12-6&end_date=2018-12-12&filter=stuff&sort=thingy'
+
+        expect(response.status).to eq(400)
+
+        error = JSON.parse(response.body, symbolize_names: true)
+
+        expect(error[:error]).to eq('filter query must = hazard')
+      end
+    end
+  end
+end
