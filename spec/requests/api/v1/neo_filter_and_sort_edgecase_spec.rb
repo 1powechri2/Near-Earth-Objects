169d8bd3b2184ff45b7c682515479b7d0beea727
Chris Powell <chris.ian.powell@gmail.com>
2018-12-13 17:32:22 -0700

    Adds test that verifies check for a bad sort query

--- spec/requests/api/v1/neo_filter_and_sort_edgecase_spec.rb
+++ spec/requests/api/v1/neo_filter_and_sort_edgecase_spec.rb
@@ -14,4 +14,17 @@ describe 'Bad requests return status errors and json error messages' do
       end
     end
   end
+  context '/api/v1/neo?start_date=2018-12-6&end_date=2018-12-12&filter=hazard&sort=thingy' do
+    it 'returns status 400' do
+      VCR.use_cassette("neos") do
+        get '/api/v1/neo?start_date=2018-12-6&end_date=2018-12-12&hazard=stuff&sort=thingy'
+
+        expect(response.status).to eq(400)
+
+        error = JSON.parse(response.body, symbolize_names: true)
+
+        expect(error[:error]).to eq('bad sort query')
+      end
+    end
+  end
 end
